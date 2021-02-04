48e3a8e7df5daf8ee567fb17c8ff4baa850ef12c
bigbig33 <joao.souza33@hotmail.com>
2021-02-04 16:02:44 -0300

    upload

---
+++ gft/StringLibrary/Produto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace StringLibrary
+{
+    public class Produto
+    {
+        private string nome;
+        private double preco;
+        private int qtd;
+
+        public string Nome { get => nome; set => nome = value; } //Propriedade da classe
+        public double Preco { get => preco; set => preco = value; } //Propriedade da classe
+
+        public int Qtd { get => qtd; set => qtd = value; }
+    }
+
+    public class Livro
+    {
+        private string autor;
+        private string tema;
+        private int qtdPag;
+
+        public string Nome { get => nome; set => nome = value; }
+        public double Preco { get => preco; set => preco = value; } 
+        public int Qtd { get => qtd; set => qtd = value; }
+        public string Autor { get => autor; set => autor = value; }
+        public string Tema { get => tema; set => tema = value; }
+        public int QtdPag { get => qtdPag; set => qtdPag = value; }
+
+    }
+
+    public class Livro
+    {
+        private string marca;
+        private string modelo;
+
+        public string Nome { get => nome; set => nome = value; }
+        public double Preco { get => preco; set => preco = value; } 
+        public int Qtd { get => qtd; set => qtd = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+
+    }
+
+    public interface IcalculadoraImposto{
+        
+    }
+    {
+        
+    }
+}
---
+++ gft/exercicio1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace gft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 0, b = 0, c = 0;
+
+            Console.Write("Digite o valor de A: ");
+            a = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de B: ");
+            b = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de C: ");
+            c = float.Parse(Console.ReadLine());
+
+            try{
+                // conta + (X1)
+
+                double x1MultAC = 4*(a*c);
+                double x1Potencia = Math.Pow(b,2);
+                double x1SubtraiPotencia = x1Potencia - x1MultAC;
+                double x1ContaRaiz = Math.Sqrt(x1SubtraiPotencia);
+                double x1Soma = -b + x1ContaRaiz;
+                double x1MultA = a * 2;
+                double x1 = x1Soma  / x1MultA;
+
+                //conta - (X2)
+
+                double x2MultAC = 4*(a*c);
+                double x2Potencia = Math.Pow(b,2);
+                double x2SubtraiPotencia = x2Potencia - x2MultAC;
+                double x2ContaRaiz = Math.Sqrt(x2SubtraiPotencia);
+                double x2Soma = -b - x2ContaRaiz;
+                double x2MultA = a * 2;
+                double x2 = x2Soma  / x2MultA;
+
+                if ((x2MultA  == 0) || (x1MultA == 0) || (x1ContaRaiz < 0) || (x2ContaRaiz < 0)){
+                    Console.Write("Impossível calcular");
+                } else {
+                    Console.WriteLine(x1.ToString("0.00000"));
+                    Console.WriteLine(x2.ToString("0.00000"));
+                }
+                
+            } catch {
+                Console.Write("Impossível calcular");
+            }
+        }
+    }
+}
